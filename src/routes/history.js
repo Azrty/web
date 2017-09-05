d9604e2cdb7ae9f57b67e9a8660542715f5fe05b
Alex <xskyzie@gmail.com>
2017-09-05 18:13:01 +0200

    Show shop name in history

--- src/routes/history.js
+++ src/routes/history.js
@@ -54,6 +54,7 @@ class History extends React.Component {
             return (
               <div key={purchase._id} id={purchase._id} className='purchase'>
                 <p>{purchase.username}</p>
+                <p>{purchase.shop}</p>
                 <p>{purchase.amount.toFixed(2)}</p>
                 <p>{purchase.description}</p>
                 <p><Moment fromNow date={new Date(purchase.date)} /></p>
