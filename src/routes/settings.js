dffa1512752f9827d9c79b80adfb83cd7c4a391e
ItsAlex <me@itsalex.fr>
2018-02-17 10:31:40 +0100

    Settings page done

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -12,12 +12,16 @@ class Settings extends Component {
       oldPassword: '',
       newPassword: '',
       newConfirmPwd: '',
-      mail: ''
+      mail: '',
+      disable: true
     }
   }
 
   onChange (e) {
-    this.setState({ [e.target.name]: e.target.value })
+    this.setState({
+      [e.target.name]: e.target.value,
+      disable: false
+    })
   }
 
   onSubmit (e) {
@@ -27,13 +31,25 @@ class Settings extends Component {
       store.notif.add('Password mismatch!', 'error')
       return
     }
+    this.setState({ disable: true })
     if (this.state.username !== '') obj.username = this.state.username
     if (this.state.newPassword !== '') obj.password = this.state.newPassword
     if (this.state.mail !== '') obj.mail = this.state.mail
     auth().put('/me', obj).then(res => {
-      console.log(res)
+      this.setState({
+        username: '',
+        oldPassword: '',
+        newPassword: '',
+        newConfirmPwd: '',
+        mail: ''
+      })
+      if (res.status === 200) store.notif.add('Change applied', 'success')
     }).catch(err => {
-      console.log(err.response)
+      if (err.response) {
+        store.notif.add(err.response.data.err, 'error')
+      } else {
+        store.notif.add('Somethings went wrong', 'error')
+      }
     })
   }
 
@@ -41,12 +57,13 @@ class Settings extends Component {
     return (
       <div id='settings'>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input className='primary-input' name='username' placeholder='Username' value={this.state.username} onChange={this.onChange.bind(this)} />
-          <input className='primary-input' name='oldPassword' type='password' placeholder='New Password' value={this.state.oldPassword} onChange={this.onChange.bind(this)} />
-          <input className='primary-input' name='newPassword' type='password' placeholder='Old Password' value={this.state.NewPassword} onChange={this.onChange.bind(this)} />
-          <input className='primary-input' name='newConfirmPwd' type='password' placeholder='Confirm Password' value={this.state.newConfirmPwd} onChange={this.onChange.bind(this)} />
           <input className='primary-input' name='mail' type='email' placeholder='Mail' value={this.state.mail} onChange={this.onChange.bind(this)} />
-          <button className='primary-btn' type='submit'>Save</button>
+          <input className='primary-input' name='username' placeholder='Username' value={this.state.username} onChange={this.onChange.bind(this)} />
+          <input className='primary-input' name='newPassword' type='password' placeholder='New Password' value={this.state.NewPassword} onChange={this.onChange.bind(this)} />
+          <input className='primary-input' name='newConfirmPwd' type='password' placeholder='Confirm Password' value={this.state.newConfirmPwd} onChange={this.onChange.bind(this)} />
+          <br />
+          <input className='primary-input' name='oldPassword' type='password' placeholder='Old Password' value={this.state.oldPassword} onChange={this.onChange.bind(this)} />
+          <button className='primary-btn' type='submit' disabled={this.state.disable}>Save</button>
         </form>
       </div>
     )
